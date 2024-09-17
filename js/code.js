b4c7fdc065ce98b80303ba3d7499d99875f4a529
Josh Miller <joshtmiller04@gmail.com>
2024-09-17 15:04:55 -0400

    more about us page and fixed register form

--- js/code.js
+++ js/code.js
@@ -280,7 +280,7 @@ function readCookie() {
     }
 
     if (userId < 0) {
-        //window.location.href = "index.html";
+        window.location.href = "index.html";
     }
     else {
         document.getElementById("userName").innerHTML = firstName;
